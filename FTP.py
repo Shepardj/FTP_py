0e2e58e20bc5adceb5c367f9af74516c3ab743e5
Jonathan Shepardson <jonshepardson@gmail.com>
2015-06-30 15:57:08 -0700

    More setup
    made a FTP.py file for our main program.

---
+++ FTP.py
@@ -0,0 +1,19 @@
+#Main python file
+
+
+#~~~~~~~~~~~~ Pre Program Stuff ~~~~~~~~~~~~~~~#
+
+import ftplib
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#~~~~~~~~~~~~ Variable/Definitions ~~~~~~~~~~~~~~~#
+
+
+
+
+#~~~~~~~~~~~~ Main Program ~~~~~~~~~~~~~~~#
+
+print("Hello World")
