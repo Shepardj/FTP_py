bc2591b1f6c781fe4a10fe2adc8a8ad7ee403537
drnida <rnida@mac.local>
2015-07-21 19:11:33 -0700

    how to test catching errors???

--- test_suite.py
+++ test_suite.py
@@ -40,7 +40,7 @@ class TestFTP(unittest.TestCase):
 
     # This test fails because I don't know how to raise exceptions...
     def test_cd_permission_denied(self):
-        with self.assertRaises(FTP.error_reply):
+        with self.assertRaises(ftplib.error_reply):
             self.ftp.cd('lost+found')
 
     def test_get_existing_to_current(self):
