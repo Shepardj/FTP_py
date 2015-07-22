dfc804d4578c560032a36414393a6717b1f7c40b
Sasha Fahrenkopf <sfahrenk@gmail.com>
2015-07-21 17:51:24 -0700

    Added one more sanity check on cd

--- test_suite.py
+++ test_suite.py
@@ -22,11 +22,16 @@ class TestFTP(unittest.TestCase):
     def test_ls_attributes(self):  # does this test do anything?
         self.assertEqual( self.ftp.ls_attributes() , None )
 
-    def test_cd_one_up(self):
+    def test_cd_one_down(self):
         self.ftp.cd('pub')
         self.assertEqual(self.ftp.pwd(), '/pub')
 
-    def test_cd_one_down(self):
+    def test_cd_two_down(self):
+        self.ftp.cd('pub')
+        self.ftp.cd('CFCA')
+        self.assertEqual(self.ftp.pwd(), '/pub/CFCA')
+
+    def test_cd_one_up(self):
         self.ftp.cd()
         self.assertEqual(self.ftp.pwd(), '/')
 
@@ -34,6 +39,7 @@ class TestFTP(unittest.TestCase):
         with self.assertRaises(NotADirectoryException):
             self.ftp.cd('foo')
 
+    # This test fails because I don't know how to raise exceptions...
     def test_cd_permission_denied(self):
         with self.assertRaises(FTP.error_reply):
             self.ftp.cd('lost+found')
